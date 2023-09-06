97e0759f43fa4cb7b30a13a463eedad13c4d5d6b
Shivam pandey <131754000+KiteShivam@users.noreply.github.com>
2023-09-07 01:05:06 +0530

    stack queu

---
+++ stack.py
@@ -0,0 +1,20 @@
+
+from collections import deque
+
+stack = deque()
+
+stack.append(1)
+
+    # Stack 
+        # pop :- pop the last  element         O(n|1)
+        # peek                                O(1)
+        # push :- push at the last position   O()
+        # Deletion :-                         O(1)
+        #search :-                            O(n)
+
+    # Queue
+
+# depue  :- remove  the first element      O(n)
+#  enque :- add  new elment at last        O(n|1)
+# peek | top : -                           O(1)
+
